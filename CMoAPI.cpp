4948a00c0078911948a95265937509fac2141844
davygrvy@pobox.com <davygrvy@pobox.com>
2024-04-18 14:08:12 -0700

    verify the loaded DLL matches our build version

--- CMoAPI.cpp
+++ CMoAPI.cpp
@@ -1,17 +1,31 @@
 #include "CMoAPI.hpp"
-#define PMD_W32SERIAL_INTERFACE
-#include "c-motion/PMDW32Ser.h"
+
 #ifdef WIN32
 #   ifdef _MSC_VER
 #	pragma comment (lib, "C-Motion.lib")
 #   endif
 #endif
 
+// The transport in use is considered to be an external hook.
+// We will use the native Win32 method for now until I write
+// a Tcl one that, will of course, be universal.  Even more
+// fun will be the oppertunity to write some new SPI/CAN/etc
+// channel driver(s) for Tcl for use when embedded on ARM. 
+//
+#define PMD_W32SERIAL_INTERFACE
+#include "c-motion/PMDW32Ser.h"
+
 CMoAPI::CMoAPI (Tcl_Interp* interp)
 {
     PMDuint32 maj, min;
 
+    // Does c-motion.h match the loaded DLL?
+    //
     PMDGetCMotionVersion(&maj, &min);
+    if ((maj != CMOTION_MAJOR_VERSION) || (min != CMOTION_MINOR_VERSION))
+    {
+	throw "C-Motion version mismatch of DLL!";
+    }
 
 #if defined PMD_CAN_INTERFACE
     // open the CAN interface at 20,000 baud and NodeID=0
